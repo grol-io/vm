adaee88a22696d1a94b2841b6e35139ed649471e
Laurent Demailly <ldemailly@gmail.com>
2025-12-22 21:12:50 -0800

    Jne: loops! (#9)
    * adding JNE and loop.asm
    
    * fix the asm to actually end after 100M
    
    * Optimize out the log debug by default, now 1B loop in <3s
    
    * adding missing files

--- cli/cli.go
+++ cli/cli.go
@@ -2,6 +2,8 @@ package cli
 
 import (
 	"flag"
+	"os"
+	"runtime/pprof"
 
 	"fortio.org/cli"
 	"fortio.org/log"
@@ -9,13 +11,43 @@ import (
 	"grol.io/vm/cpu"
 )
 
+func memProfile(fname string) {
+	f, err := os.Create(fname)
+	if err != nil {
+		log.Errf("can't open file for memory profile: %v", err)
+		return
+	}
+	err = pprof.WriteHeapProfile(f)
+	if err != nil {
+		log.Errf("can't write memory profile: %v", err)
+	}
+	f.Close()
+}
+
 func Main() int {
 	cli.CommandBeforeFlags = true
 	cli.MinArgs = 1
 	cli.MaxArgs = -1
 	cli.ArgsHelp = "files...\nwhere command is one of: compile, run"
+	cpuProf := flag.String("profile-cpu", "", "write CPU profile to file")
+	memProf := flag.String("profile-mem", "", "write memory profile to file")
 	cli.Main()
 	log.Debugf("Command: %s, Args: %v", cli.Command, flag.Args())
+	if *cpuProf != "" {
+		f, err := os.Create(*cpuProf)
+		if err != nil {
+			return log.FErrf("can't open file for cpu profile: %v", err)
+		}
+		err = pprof.StartCPUProfile(f)
+		if err != nil {
+			return log.FErrf("can't start cpu profile: %v", err)
+		}
+		log.Infof("Writing cpu profile to %s", *cpuProf)
+		defer pprof.StopCPUProfile()
+	}
+	if *memProf != "" {
+		defer memProfile(*memProf)
+	}
 	switch cli.Command {
 	case "compile":
 		return asm.Compile(flag.Args()...)
---
+++ programs/loop.asm
@@ -0,0 +1,6 @@
+    # 1 billion iterations
+    load 1_000_000_000
+loop:
+    add -1
+    jne loop
+    abort
