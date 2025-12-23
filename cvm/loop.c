6d15d954e1e9690f985413a52457e1f1a3748ce7
Jorropo <jorropo.pgm@gmail.com>
2025-12-24 00:19:15 +0100

    lower overhead from optimization prevention mesures in loop.c
    This reduce the overhead from `volatile` while keeping the loop around.

--- cvm/loop.c
+++ cvm/loop.c
@@ -3,9 +3,10 @@
 #include <stdio.h>
 
 int main(void) {
-    volatile int64_t counter = 1000000000;
-    while (counter !=0) {
+    int64_t counter = 1000000000;
+    while (counter != 0) {
         counter--;
+        asm volatile ("");
     }
     printf("Counter: %" PRId64 "\n", counter);
     return 0;
