f9851bcb0a5eb4898aeaa5920c9b5aa6c0ef6452
Laurent Demailly <ldemailly@gmail.com>
2025-12-23 15:06:07 -0800

    prevent loop optimization in pure C

--- cvm/loop.c
+++ cvm/loop.c
@@ -1,11 +1,12 @@
 #include <stdint.h>
+#include <inttypes.h>
 #include <stdio.h>
 
 int main(void) {
-    int64_t counter = 1000000000;
+    volatile int64_t counter = 1000000000;
     while (counter !=0) {
         counter--;
     }
-    printf("Counter: %lld\n", counter);
+    printf("Counter: %" PRId64 "\n", counter);
     return 0;
 }
