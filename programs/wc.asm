da8df0aecfc664ef3b9a54b9922c181a43aa3512
Laurent Demailly <ldemailly@gmail.com>
2026-01-03 17:44:57 -0800

    Slightly better label and comment in wc.asm

--- programs/wc.asm
+++ programs/wc.asm
@@ -2,7 +2,7 @@
 ; Compile with: vm compile programs/wc.asm programs/itoa.asm
 
     ; reserve 256 bytes for buffer (b1-b31+buf)=32 words=32*8=256 bytes
-    Var count bytes_read b1 b2 b3 b4 b5 b6 b7 b8 b9 b10 b11 b12 b13 b14 b15 b16 b17 b18 b19 b20 b21 b22 b23 b24 b25 b26 b27 b28 b29 b30 b31 buf; buf LAST so buffer extends forward
+    Var count bytes_read b1 b2 b3 b4 b5 b6 b7 b8 b9 b10 b11 b12 b13 b14 b15 b16 b17 b18 b19 b20 b21 b22 b23 b24 b25 b26 b27 b28 b29 b30 b31 buf; buf extends into the b*
     LoadI 0
     StoreS count
 
@@ -15,9 +15,9 @@ read_loop:
 count_loop:
     IncrS -1 bytes_read
     LoadSB buf bytes_read
-    JNE '\n' skip_count
-    IncrS 1 count
-skip_count:
+    JNE '\n' not_newline
+    IncrS 1 count ; found newline, count it
+not_newline:
     LoadS bytes_read
     JGT 0 count_loop
     JumpR read_loop
