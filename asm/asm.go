0da19a003f9e4bf4baed371d9e160ac9bc0e96e6
Laurent Demailly <ldemailly@gmail.com>
2026-01-07 17:32:49 -0800

    move it to simpleExpr.go

--- asm/asm.go
+++ asm/asm.go
@@ -188,41 +188,6 @@ func (r *Resolver) Const(name string) (int64, bool) {
 	return v, ok
 }
 
-func (r *Resolver) ResOperator(op Operator, left, right string) (int64, error) {
-	var leftV int64
-	if left != "" {
-		// to handle "-x" "+x" (will also yield 0 on *x etc but whatever)
-		var err error
-		leftV, err = r.ResValue(left)
-		if err != nil {
-			return 0, err
-		}
-	}
-	rightV, err := r.ResValue(right)
-	if err != nil {
-		return 0, err
-	}
-	var res int64
-	switch op {
-	case Plus:
-		res = leftV + rightV
-	case Minus:
-		res = leftV - rightV
-	case Mult:
-		res = leftV * rightV
-	case Div:
-		res = leftV / rightV
-	case Mod:
-		res = leftV % rightV
-	case LeftShift:
-		res = leftV << rightV
-	case RightShift:
-		res = leftV >> rightV
-	}
-	log.LogVf("Evaluated expression: %s %s %s = %d", left, op, right, res)
-	return res, nil
-}
-
 func (r *Resolver) ResValue(strVal string) (int64, error) {
 	if v, ok := r.Const(strVal); ok {
 		return v, nil
