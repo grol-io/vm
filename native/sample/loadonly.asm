ac11e35804c1e2794e56dd96c12e1775ab4ac8f3
Laurent Demailly <ldemailly@gmail.com>
2026-01-23 10:42:48 -0800

    Get rid of loadonly

--- native/sample/loadonly.asm
+++
@@ -1,9 +0,0 @@
-; loadonly.asm: Test LoadR only (no write to data segment)
-; If this works but counter.asm doesn't, the issue is StoreR / data segment writability
-
-    LoadR value     ; Load value into A (should be 42)
-    ; A = 42, exit with 42 (exit code proves LoadR worked)
-    Sys Exit 42
-
-value:
-    data 42         ; writable data (will be in data segment)
